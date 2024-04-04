cbb203dc965ade50c0082137adb58d95a3702ca4
olegz <ozenderm@lakeahu.ca>
2024-04-04 15:39:03 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var menu = document.getElementById('slide-out-menu');
+    menu.style.right = '-100%';
+
+    AOS.init();
+}
+
+document.getElementById('menu-button').addEventListener('click', function() {
+    var menu = document.getElementById('slide-out-menu');
+    if (menu.style.right === '-100%') {
+        menu.style.right = '0';
+    } else {
+        menu.style.right = '-100%';
+    }
+});
+
+document.getElementById('menu-button').addEventListener('click', function() {
+    this.classList.toggle('clicked');
+    document.getElementById('slide-out-menu').classList.toggle('open');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for submitting a form, our team will respond in between 2-5 business days");
+});
